76cdbcc6b023f18d897d702cf6cc9e6f301fbc82
spereira02 <spereira@ethz.ch>
2026-03-06 14:29:58 +0100

    cleanup

--- include/project_settings.h
+++ include/project_settings.h
@@ -2,8 +2,6 @@
 #define PROJECT_SETTINGS_H
 
 // Global settings
-#define WIFI_SSID           "Your_WiFi_SSID"
-#define WIFI_PASS           "Your_WiFi_PASSWORD"
 #define ROS_NAMESPACE       "imu_node"
 
 #endif
